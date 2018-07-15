fc25ae94f795098bbdd4b852f4ad78d84e36348d
Christopher Metzger <chrismetzgr@gmail.com>
2018-07-15 21:06:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,11 @@ function ternaryCheckCity(destination){
 }
 
 function switchOnCharmFromTip(tip){
-  switch(tip)
+  switch (tip) {
   case 'generous:
     return 'Thank you so much.';
     break;
   case 'not as generous'
     return ''
+  }
 }

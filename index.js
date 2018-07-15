a0aa3170121088f8c62d3a27caa965784f78fb2e
Christopher Metzger <chrismetzgr@gmail.com>
2018-07-15 20:53:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scuberGreetingForFeet(number){
   let response 
   if (number <= 400)
   response = "This one is on me!"
-  else if (number =< 2500 )
+  else if (number <= 2500 )
   response = "I will gladly take your thirty bucks"
   
 }
